7fe5028d3211864d9db0768284fd4f2b06753263
Tobias A. Rau <github@tar-it.com>
2023-12-04 20:32:55 +0100

    Add comments

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,8 @@
 use std::fs::File;
 use std::io::Read;
 
-
 fn main() {
-    // Open the file in read-only mode
+    // Open the file named "input.txt" in read-only mode
     let file = File::open("input.txt");
 
     // Handle the result of opening the file
@@ -18,33 +17,39 @@ fn main() {
             println!("Part 2: {}", part_2(&input));
         }
         Err(e) => {
+            // Print an error message if there was an issue opening the file
             eprintln!("Error opening the file: {}", e);
         }
     }
 }
 
-
+// Function for solving Part 1 of the problem
 fn part_1(input: &str) -> u32 {
+    // For each line in the input, calculate a value based on the digits found in the line
     input.lines().map(|line| {
+        // Extract the first and last digit found in the line and perform a calculation
         line.chars().find_map(|c| c.to_digit(10)).unwrap() * 10 +
         line.chars().rev().find_map(|c| c.to_digit(10)).unwrap()
-    }).sum()
+    }).sum()  // Sum up the calculated values for all lines
 }
 
+// Define an array mapping word representations of digits to their numeric values
 const DIGITS: [[&str; 2]; 9] = [
     ["one", "1"], ["two", "2"], ["three", "3"], ["four", "4"], ["five", "5"], ["six", "6"], ["seven", "7"],
     ["eight", "8"], ["nine", "9"],
 ];
 
-
+// Function for solving Part 2 of the problem
 fn part_2(input: &str) -> usize {
+    // For each line in the input, calculate a value based on the positions of specific digits
     input.lines().map(|line| {
+        // For each digit, find its position in the line and calculate a value
         DIGITS.iter().enumerate().flat_map(|(n, &digits)| {
             digits.into_iter().filter_map(move |digit| line.find(digit).map(|pos| (n + 1, pos)))
         }).min_by_key(|&(_, pos)| pos).unwrap().0 * 10 +
+        // Repeat the process for the reverse positions and calculate a value
         DIGITS.iter().enumerate().flat_map(|(n, &digits)| {
             digits.into_iter().filter_map(move |digit| line.rfind(digit).map(|pos| (n + 1, pos)))
         }).max_by_key(|&(_, pos)| pos).unwrap().0
-    }).sum()
+    }).sum()  // Sum up the calculated values for all lines
 }
-
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,45 +2,55 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
-
 fn main() {
+    // Read the contents of the "input.txt" file into a String
     let mut input = String::new();
-    File::open("input.txt").expect("Unable to open input.txt").read_to_string(&mut input).expect("Unable to read input.txt");
+    File::open("input.txt").expect("Unable to open input.txt")
+        .read_to_string(&mut input).expect("Unable to read input.txt");
+
+    // Call the part_1 and part_2 functions with the input and print the results
     println!("Part 1: {}", part_1(&input));
     println!("Part 2: {}", part_2(&input));
 }
 
-
+// Function for solving Part 1 of the problem
 fn part_1(input: &str) -> usize {
+    // Define a HashMap mapping color names to their allowed counts
     let colors: HashMap<_, _> = [("red", 12), ("green", 13), ("blue", 14)].into_iter().collect();
 
+    // For each line in the input, check if the count of each color is within the allowed limits
     input.lines().enumerate().filter_map(|(n, line)| {
         let mut iter = line.split_ascii_whitespace().skip(2);
         while let Some(x) = iter.next() {
             let count: u32 = x.parse().unwrap();
             let color = iter.next().unwrap().trim_end_matches([',', ';']);
+            // If the count exceeds the allowed limit, return None
             if count > colors[color] {
                 return None;
             }
         }
+        // Return the line number + 1 for lines that meet the criteria
         Some(n + 1)
-    }).sum()
+    }).sum()  // Sum up the line numbers for valid lines
 }
 
-
+// Function for solving Part 2 of the problem
 fn part_2(input: &str) -> u32 {
+    // For each line in the input, calculate the product of maximum counts for each color
     input.lines().map(|line| {
+        // Initialize a HashMap to track maximum counts for each color
         let mut colors: HashMap<_, _> = [("red", 0), ("green", 0), ("blue", 0)].into_iter().collect();
         let mut iter = line.split_ascii_whitespace().skip(2);
         while let Some(x) = iter.next() {
             let count: u32 = x.parse().unwrap();
             let color = iter.next().unwrap().trim_end_matches([',', ';']);
+            // Update the maximum count for the current color if necessary
             let max = colors.get_mut(color).unwrap();
             if count > *max {
                 *max = count;
             }
         }
+        // Calculate the product of maximum counts for all colors
         colors.into_iter().map(|(_, count)| count).product::<u32>()
-    }).sum()
+    }).sum()  // Sum up the calculated products for all lines
 }
-
